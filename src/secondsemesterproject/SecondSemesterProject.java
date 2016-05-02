5e158f0ce72da9637ba8daec4ec96a335b2ea7d8
bobkoobg <boyko.surlev@gmail.com>
2016-05-02 13:53:49 +0200

    Initial commit

---
+++ src/secondsemesterproject/SecondSemesterProject.java
@@ -0,0 +1,13 @@
+package secondsemesterproject;
+
+public class SecondSemesterProject {
+
+    public static void main( String[] args ) {
+        hello( "Dimitrii" );
+    }
+
+    private static void hello( String name ) {
+        System.out.println( "Hello it's " + name );
+    }
+
+}
